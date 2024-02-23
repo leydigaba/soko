6a2123947af30c12628ae140bc64aa6b8bb81faa
leydigaba <160621392+leydigaba@users.noreply.github.com>
2024-02-23 00:41:46 -0600

    Create cambio.py

---
+++ cambio.py
@@ -0,0 +1,4 @@
+mapa=[3,4,4,0,4,4,4,3]
+mi_lista[2], mi_lista[3] = mi_lista[3], mi_lista[2] #Lee el valor en la posición 2 de mi_lista y lo asigna al elemento en la posición 3 de mi_lista
+mapa[3], mapa[4] = mapa[4], mapa[3] # ee el valor en la posición 4 de mi_lista (que aún no ha sido modificado) y lo asigna al elemento en la posición 3 de mi_lista.
+print(mapa)
